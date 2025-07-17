d08f3e7a14acd2a1c41d380279e8e106c5fc9460
Kafiling <panthawit.k@gmail.com>
2025-07-17 23:41:10 +0700

    Replacing Template

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -33,22 +33,52 @@ const url = require('url');
 // Server
 
 // Syncournous code on top-level is ok
+const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8')
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
-console.log(dataObj)
+
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%FROM%}/g, product.from)
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    output = output.replace(/{%DESCRIPTION%}/g, product.description)
+    output = output.replace(/{%ID%}/g, product.id)
+
+    if (!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not_organic')
+    }
+
+    return output
+}
+
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);
     const pathName = req.url
+
+    //Overview Page
+
     if (pathName === '/overview' || pathName === '/') {
-        res.end('This is Overview')
-    } else if (pathName === '/product') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+        res.end(output)
+    }
+    //Product Page
+    else if (pathName === '/product') {
         res.end('This is Product')
     }
+    // API
     else if (pathName === '/api') {
         res.writeHead(200, { 'content-type': 'application/json' })
         res.end(data)
     }
+    // Not Found
     else {
         //writeHead(statusNumber,HeaderObject)
         //IMPORTANT header must be written before respond content
